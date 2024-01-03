6ec7708602c37a40ec96070f11bb1deda74eafe4
Garrett Gregor <garrett.gregor@gmail.com>
2024-01-03 16:48:17 -0700

    feat: startGPS

--- practice.js
+++ practice.js
@@ -1,11 +1,19 @@
 function findRemainder(num1, num2) {
   var remainder = num1 % num2;
   return remainder
+};
+
+function startGPS(currentLocation, newLocation) {
+  if (currentLocation != newLocation) {
+    return `Taking you from ${currentLocation} to ${newLocation}!  Getting directions now!`
+  } else {
+    return `You are already at the ${newLocation}!  Would you like to go somewhere else?`
+  }
 }
 
 module.exports = {
   findRemainder,
-  // startGPS,
+  startGPS,
   // createUser,
   // makePizza
 }
